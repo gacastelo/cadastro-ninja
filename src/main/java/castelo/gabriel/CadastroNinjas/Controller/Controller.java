e491c5d84c51f31ca266fb5c7c613b3364593e92
gacastelo <gabriel.castelo5949@gmail.com>
2026-03-19 23:32:13 -0300

    Criando o Controller

---
+++ src/main/java/castelo/gabriel/CadastroNinjas/Controller/Controller.java
@@ -0,0 +1,14 @@
+package castelo.gabriel.CadastroNinjas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boas")
+    public String boasVindas(){
+      return "Seja bem vindo";
+    }
+}
