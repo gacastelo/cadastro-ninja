ed65b22a6defdd3a98c7a8e05d55b006bffff688
gacastelo <gabriel.castelo5949@gmail.com>
2026-03-20 22:20:57 -0300

    feat: Add Repository for Missoes and Ninja Entities

---
+++ src/main/java/castelo/gabriel/CadastroNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package castelo.gabriel.CadastroNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
---
+++ src/main/java/castelo/gabriel/CadastroNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package castelo.gabriel.CadastroNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
