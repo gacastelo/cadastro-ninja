fc4beafe106b02d348a0f1434a2e36e6a77efdf9
gacastelo <gabriel.castelo5949@gmail.com>
2026-03-21 23:57:19 -0300

    feat: add routes for NinjaController

--- src/main/java/castelo/gabriel/CadastroNinjas/Ninjas/NinjaController.java
+++ src/main/java/castelo/gabriel/CadastroNinjas/Ninjas/NinjaController.java
@@ -1,14 +1,32 @@
 package castelo.gabriel.CadastroNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
-    @GetMapping("/boas")
-    public String boasVindas(){
-      return "Seja bem vindo";
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas() {
+        return "Todos os Ninjas";
+    }
+
+    @GetMapping("/todosID")
+    public String mostraNinjaPorID() {
+        return "Ninja por Id";
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja por ID";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Deletar Ninja por ID";
     }
 }
