d2d0e41255131ba093d64193c50ea4f65b0ca90f
gacastelo <gabriel.castelo5949@gmail.com>
2026-03-22 00:03:45 -0300

    feat: create mock routes for MissoesController

--- src/main/java/castelo/gabriel/CadastroNinjas/Missoes/MissoesController.java
+++ src/main/java/castelo/gabriel/CadastroNinjas/Missoes/MissoesController.java
@@ -1,9 +1,25 @@
 package castelo.gabriel.CadastroNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao Criada";
+    }
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao Alterada";
+    }
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao Deletadada";
+    }
 }
