03dd1cba7dd2ee1104fc8b2b01b4c61c3a5ccca8
gacastelo <gabriel.castelo5949@gmail.com>
2026-03-30 23:29:07 -0300

    feat: add HTML response for errors

---
+++ src/main/java/castelo/gabriel/CadastroNinjas/UI/MyErrorController.java
@@ -0,0 +1,25 @@
+package castelo.gabriel.CadastroNinjas.UI;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            model.addAttribute("status", statusCode);
+            model.addAttribute("error", HttpStatus.valueOf(statusCode).getReasonPhrase());
+        }
+        return "error";
+    }
+}
